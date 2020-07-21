4cb8915efe52e557512500082569154d0bf65e83
unknown <shivasankari3101@gmail.com>
2020-07-21 20:35:32 +0530

    addition

---
+++ js/ajax.js
@@ -0,0 +1,38 @@
+(function(global){
+
+	var ajax={};
+
+	function getRequestObject(){
+		if(window.XMLHttpRequest){
+			return(new XMLHttpRequest());
+		}
+
+		else if(window.ActiveXObject){
+			return(new ActiveXObject("Microsoft.XMLHTTP"));
+		}
+
+		else{
+			global.alert("Ajax is not supported");
+			return (null);
+		}
+	}
+
+    ajax.sendGetRequest = function(requestUrl,responseHandler){
+    	var request=getRequestObject();
+    	var request.onreadystatechange =
+    	function (){
+    		handleResponse(request,responseHandler);
+    	};
+    	request.open("GET",requestUrl,true);
+    	request.send(null);
+   	}
+
+   	function handleResponse (request,responseHandler){
+       if((request.readystate==4) && (request.status==200)){
+       	 return(request.responseText);
+       }
+   	}
+
+   	global.$ajax=ajax;
+
+   })(window);
